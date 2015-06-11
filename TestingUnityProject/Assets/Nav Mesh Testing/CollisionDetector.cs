c306b0d79678f795153525314a7e06a2171eff9c
Jennifer Lather <jennifer.lather@aditazz.com>
2015-06-11 14:48:55 -0700

    Testing with Unity Project again

---
+++ TestingUnityProject/Assets/Nav Mesh Testing/CollisionDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public MeshCollider spot;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter (){
+
+	}
+	// Update is called once per frame
+	void Update () {
+		OnCollisionEnter ();
+	}
+}
---
+++ TestingUnityProject/Assets/Nav Mesh Testing/MoveTo.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+	//public Transform goal1;
+	public Transform[] points;
+	public GameObject[] rooms;
+	private int destPoint = 0;
+	private NavMeshAgent agent;
+
+
+	void Start () {
+	
+		agent = GetComponent<NavMeshAgent> ();
+
+		GotoNextPoint();
+
+	}
+	
+	void GotoNextPoint(){
+
+		if (points.Length == 0)
+			return;
+
+		agent.destination = points[destPoint].position;
+
+		destPoint = (destPoint + 1) % points.Length;
+	
+	}
+
+	IEnumerator Wait(){
+			
+			yield return new WaitForSeconds (2);
+
+	}
+	
+
+	void Update(){
+
+		if (agent.remainingDistance < 0.5f) 
+//			StartCoroutine ("Wait");
+//			agent.Resume ();
+			GotoNextPoint();
+
+
+	}
+
+}
